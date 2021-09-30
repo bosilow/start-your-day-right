68203ff7133c32ea1cb7a07f5ebaf71f7e5a0311
bosilow <bosiljkostojanovic@gmail.com>
2021-09-30 16:46:47 +0200

    added responsive design

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
 const Header = ({ refreshPage }) => (
   <header className="header">
-    <h1 className="header-title" onClick={refreshPage}>
+    <div className="header-title" onClick={refreshPage}>
       START YOUR DAY RIGHT
-    </h1>
-    <p>
-      Are you feeling rough that you had to wake up early to work/go to school
-      and perhaps you need some emotional boost?
-    </p>
-    <p>You came to the right place!</p>
+    </div>
+    <div className="sub-title">
+      <p>
+        Are you feeling rough that you had to wake up early to work/go to school
+        and perhaps you need some emotional boost?
+      </p>
+      <p>You came to the right place!</p>
+    </div>
   </header>
 );
 
